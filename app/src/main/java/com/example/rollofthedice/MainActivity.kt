2b6dd62aa9200ac9917cd2cc243f87dadf71b0da
P-Irene <procopioirene1@gmail.com>
2025-04-10 11:45:55 +0200

    Creazione Intent - SecondActivity completata - activity_second completata - ThirdActivity creata - activity_third creata

--- app/src/main/java/com/example/rollofthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollofthedice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rollofthedice
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -20,8 +21,10 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "LANCIO DEL DADO!", Toast.LENGTH_LONG).show()
         })
 
-        fun showMessage(view : View){
-            Toast.makeText(this, "Try again!", Toast.LENGTH_LONG).show()
-        }
+        val mioIntent : Intent = Intent(this, SecondActivity::class.java)
+        val mioRandom = (1..6).random()
+        mioIntent.putExtra("MESSAGE", "NUMERO ESTRATTO: $mioRandom")
+        mioIntent.putExtra("RANDOM", mioRandom)
+        startActivity(mioIntent)
     }
 }
---
+++ app/src/main/java/com/example/rollofthedice/SecondActivity.kt
@@ -0,0 +1,37 @@
+package com.example.rollofthedice
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+        var msg = intent.getStringExtra("MESSAGE")
+        val textView = findViewById<TextView>(R.id.textViewActivitySecond)
+        textView.text = msg
+
+        var imageViewSecond = findViewById<ImageView>(R.id.imageViewActivitySecond)
+        var random = intent.getIntExtra("RANDOM", -1)
+
+        var resource = when (random) {
+            1 -> R.drawable.dice_face_1
+            2 -> R.drawable.dice_face_2
+            3 -> R.drawable.dice_face_3
+            4 -> R.drawable.dice_face_4
+            5 -> R.drawable.dice_face_5
+            6 -> R.drawable.dice_face_6
+            else -> {R.drawable.dices}
+        }
+        imageViewSecond.setImageResource(resource)
+        Log.d("SECOND", "FINITO DISEGNO DADO")
+    }
+}
